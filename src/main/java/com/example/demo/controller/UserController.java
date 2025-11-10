f40292e87d6dfa8fa949f63f5ac79f6c8bab25ad
Maria Danescu <mariadanescu@Marias-MacBook-Pro.local>
2025-11-10 15:06:56 +0200

    add changes for container

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,8 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
-
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -22,9 +23,24 @@ public class UserController {
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
+    public User createUser(@RequestBody Map<String, String> body) {
+        String name = body.get("name");
+        String dateTimeStr = body.get("scheduledDateTime"); // așteptat ca "2025-10-30T14:30"
+        LocalDateTime scheduledDateTime = LocalDateTime.parse(dateTimeStr);
+        User user = new User(name, scheduledDateTime);
+        return  userRepository.save(user);
+    }
+
+    @PutMapping("/{id}/approve")
+    public User approveUser(@PathVariable Long id) {
+        User user = userRepository.findById(id).orElseThrow();
         return userRepository.save(user);
     }
 
+    @PutMapping("/{id}/reject")
+    public User rejectUser(@PathVariable Long id) {
+        User user = userRepository.findById(id).orElseThrow();
+        return userRepository.save(user);
+    }
 
 }
